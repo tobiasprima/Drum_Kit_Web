66b8c67034e63106398d51cdfeb7fa7c4d355ed9
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-24 11:29:40 +0700

    Added background to buttons

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+var audio = new Audio('sounds/tom-1.mp3');
+
 for (var i = 0; i < document.querySelectorAll('.drum').length; i++) {
     document.querySelectorAll('button')[i].addEventListener("click", function () {
-        alert('I got clicked!');
+        audio.play();
     });
 }
 
